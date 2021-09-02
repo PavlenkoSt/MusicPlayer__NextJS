e7e03460debca0c3ad5ce49625e7299ea178b451
Stanislav <verbatim8872@gmail.com>
2021-09-02 16:06:53 +0300

    add main layout and header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import s from '../styles/Header.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router'
 
 const Header = () => {
+
+    const path = useRouter().pathname
+
+    const links = [
+        { link: '/', text: 'Main', active: path === '/' },
+        { link: '/track-list', text: 'Track List', active: path === '/track-list' },
+        { link: '/upload', text: 'Upload', active: path === '/upload' }
+    ]
+
+    const renderLinks = links.map(link => <div key={link.link} className={`${s.link} ${link.active ? s.active : ''}`}>
+            <Link href={link.link}>{link.text}</Link>
+    </div>)
+
     return (
         <header className={s.header}>
             <Image
@@ -11,7 +26,7 @@ const Header = () => {
                 height='75'
             />
             <nav className={s.nav}>
-
+                { renderLinks }
             </nav>
         </header>
     )
