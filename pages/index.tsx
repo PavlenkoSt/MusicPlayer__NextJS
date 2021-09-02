9afc7ee6c702c1271122fd94a55bce92367210b4
Stanislav <verbatim8872@gmail.com>
2021-09-02 16:29:34 +0300

    add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import MainLayout from '../layouts/MainLayout'
+import s from '../styles/Home.module.scss'
 
 
 const Home: NextPage = () => {
   return (
-    <MainLayout title='Music Player | Main'>
-        
+    <MainLayout title='Main'>
+      <div className={s.container}>
+        <h1 className={s.main}>Welcome to best music player in the World!</h1>
+        <h2 className={s.second}>It's full stack application, when you can find all best tracks!</h2>
+        <div className={s.btn}>
+          <Link href='/track-list'>Get Started!</Link>
+        </div>
+      </div>
     </MainLayout>
   )
 }
---
+++ pages/track-list/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../layouts/MainLayout'
+
+const index = () => {
+    return (
+        <MainLayout title='Track List'>
+            
+        </MainLayout>
+    )
+}
+
+export default index
