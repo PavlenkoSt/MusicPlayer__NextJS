133bc8aaa8e36b41f27ba6770c11184cf2faa14f
Stanislav <verbatim8872@gmail.com>
2021-09-07 13:00:37 +0300

    add player actions

--- store/actions/player.ts
+++ store/actions/player.ts
@@ -1,9 +1,15 @@
+import { playerActionCreatorTypes } from './../types/player'
 import { playerActionTypes } from '../types/player'
 import { ITrack } from './../../types/track'
 
 
 const playerActions = {
-    setTrack: (payload: ITrack | null) => ({ type: playerActionTypes.SET_TRACK, payload })
+    setTrack: (payload: ITrack | null): playerActionCreatorTypes => ({ type: playerActionTypes.SET_TRACK, payload }),
+    play: (): playerActionCreatorTypes => ({ type: playerActionTypes.PLAY }),
+    pause: (): playerActionCreatorTypes => ({ type: playerActionTypes.PAUSE }),
+    setDuration: (payload: number): playerActionCreatorTypes => ({ type: playerActionTypes.SET_DURATION, payload }),
+    setCurrentTime: (payload: number): playerActionCreatorTypes => ({ type: playerActionTypes.SET_CURRENT_TIME, payload }),
+    setVolume: (payload: number): playerActionCreatorTypes => ({ type: playerActionTypes.SET_VOLUME, payload }),
 }
 
 export default playerActions
--- store/types/player.ts
+++ store/types/player.ts
@@ -9,13 +9,46 @@ export type PlayerState = {
 }
 
 export enum playerActionTypes {
-    SET_TRACK = 'SET_TRACK'
+    SET_TRACK = 'SET_TRACK',
+    SET_DURATION = 'SET_DURATION',
+    SET_CURRENT_TIME = 'SET_CURRENT_TIME',
+    SET_VOLUME = 'SET_VOLUME',
+    PAUSE = 'PAUSE',
+    PLAY = 'PLAY',
 }
 
 type setTrackType = {
     type: playerActionTypes.SET_TRACK,
-    payload: ITrack
+    payload: ITrack | null
+}
+
+type setPause = {
+    type: playerActionTypes.PAUSE
+}
+
+type setPlay = {
+    type: playerActionTypes.PLAY
+}
+
+type setCurrentTime = {
+    type: playerActionTypes.SET_CURRENT_TIME
+    payload: number
+}
+
+type setDuration = {
+    type: playerActionTypes.SET_DURATION
+    payload: number
+}
+
+type setVolume = {
+    type: playerActionTypes.SET_VOLUME
+    payload: number
 }
 
 export type playerActionCreatorTypes = 
-    setTrackType 
+    setTrackType |
+    setPause | 
+    setPlay | 
+    setCurrentTime |
+    setDuration |
+    setVolume
